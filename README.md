f6a267196e77679c28b780be5748ea15ae8938e5
CofChips <qiwei.mod@gmail.com>
2020-07-09 15:27:41 -0700

    changed approach to async await to display badges

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
+![GitHub repo size](https://img.shields.io/github/repo-size/CofChips/code_quiz)
 [![start with why](https://img.shields.io/badge/start%20with-why%3F-brightgreen.svg?style=flat)](http://www.ted.com/talks/simon_sinek_how_great_leaders_inspire_action)
-# Good Readme Generator
+# something
 ## Description
-This project is a node app that runs in your terminal. User is prompted with a series of questions utilizing inquirer NPM and a readme is generated once the process is completed.
+
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
@@ -10,14 +11,15 @@ This project is a node app that runs in your terminal. User is prompted with a s
 * [Tests](#tests)
 * [Questions](#questions)
 ## Installation
-Download package.json and index.js file and place them in the same directory.  From your terminal, enter into the directory and type "npm install".  The development environment is setup.  To run the app, simply type in "node index.js" after entering into the directory where your index.js is located.
+
 ## Usage
-To run the app, simply type...
+
 ## License
-MIT
+
 ## Contributing
-No
+
 ## Tests
-Pending
+
 ## Questions
-How did you find this page?
+
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/dwyl/esta/issues)
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } = require("constants");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
 
 const contributions = "[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/dwyl/esta/issues)"
 
 const why = "[![start with why](https://img.shields.io/badge/start%20with-why%3F-brightgreen.svg?style=flat)](http://www.ted.com/talks/simon_sinek_how_great_leaders_inspire_action)"
 
-const hits = "[![HitCount](http://hits.dwyl.com/CofChips/weather_dashboard.svg)](http://hits.dwyl.com/CofChips/weather_dashboard)"
+const userData = {};
 
-// let repo = "![GitHub repo size](https://img.shields.io/github/repo-size/"+username+"/"+repo+")"
 
 inquirer.prompt([
     {
@@ -33,7 +35,8 @@ inquirer.prompt([
         choices: [
             "contributions welcome",
             "start with why",
-            "repo size"
+            "repo size",
+            "dependencies"
         ]
     },
     {
@@ -74,73 +77,212 @@ inquirer.prompt([
 
 ]).then(data => {
     console.log(data);
-    console.log("badge 0: " + data.badge[0])
-    console.log("badge 1: " + data.badge[1])
     const filename = "README.md";
 
-    if (data.badge[0] === "contributions welcome" && data.badge[1] === "start with why") {
-        fs.writeFile(filename, contributions + '\n' + why + '\n' + "# " + data.project_title + '\n', function (err) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log("Badges and title logged!");
-            }
-        });
-    }
-    else if (data.badge[0] === "contributions welcome") {
+    writeFileAsync("TEST.md","# "+data.project_title+'\n');
 
-        fs.writeFile(filename, contributions + '\n' + "# " + data.project_title + '\n', function (err) {
-            if (err) {
-                console.log(err);
+    async function combineData(){
+        try{
+            if (data.badge.includes("contributions welcome")) {
+                await appendFileAsync("TEST.md",contributions+'\n')
             }
-            else {
-                console.log("Contribution badge and title logged!");
+            if (data.badge.includes("start with why")) {
+                await appendFileAsync("TEST.md",why+'\n')
             }
-        })
-    }
-    else if (data.badge[0] === "start with why") {
-        fs.writeFile(filename, why + '\n' + "# " + data.project_title + '\n', function (err) {
-            if (err) {
-                console.log(err);
+            const repo = await "![GitHub repo size](https://img.shields.io/github/repo-size/" + data.username + "/" + data.repo + ")"
+            if (data.badge.includes("repo size")) {
+                await appendFileAsync("TEST.md",repo+'\n')
             }
-            else {
-                console.log("Why badge and title logged!");
+            const depend = await "![David](https://img.shields.io/david/" + data.username + "/" + data.repo + ")"
+            if (data.badge.includes("dependencies")) {
+                await appendFileAsync("TEST.md",depend+'\n')
             }
-        })
-    }
-    else {
-        fs.writeFile(filename, "# " + data.project_title + '\n', function (err) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log("Title logged!");
-            }
-        })
-    }
-    fs.appendFile(filename, "## " + "Description" + '\n' + data.description + '\n', function (err) {
-        if (err) {
+        }
+        catch(err) {
             console.log(err);
-        }
-        else {
-            console.log("Description logged!");
-        }
-    })
 
-    fs.appendFile(filename, "## " + "Table of Contents" + '\n' + "* [Installation](#installation)" + '\n'+ "* [Usage](#usage)" + '\n' + "* [License](#license)" + '\n' + "* [Contributing](#contributing)" + '\n' + "* [Tests](#tests)" + '\n' + "* [Questions](#questions)" + '\n'+"## " + "Installation" + '\n' + data.install + '\n'+"## " + "Usage" + '\n' + data.usage + '\n'+"## " + "License" + '\n' + data.license + '\n'+"## " + "Contributing" + '\n' + data.contributing + '\n'+"## " + "Tests" + '\n' + data.tests + '\n'+"## " + "Questions" + '\n' + data.questions + '\n', function (err) {
-        if (err) {
-            console.log(err);
         }
-        else {
-            console.log("Table of contents, installation, usage, license, contributing, tests, and questions logged!");
-        }
-    })
-//     * [Installation](#installation)
-// * [Usage](#usage)
-// * [License](#license)
-// * [Contributing](#contributing)
-// * [Tests](#tests)
-// * [Questions](#questions)
-// + "## " + "Installation" + '\n' + data.install + '\n'
+    }
+    combineData();
+
+
 })
+
+
+
+
+
+//     fs.writeFile(filename, "", function (err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             console.log("File created");
+//         }
+//     });
+
+//     const username = data.username;
+//     const repository = data.repo;
+//     const repo = "![GitHub repo size](https://img.shields.io/github/repo-size/" + username + "/" + repository + ")"
+
+//     // const depend = "![David](https://img.shields.io/david/" + username + "/" + repository + ")"
+
+
+
+//     const badges = [];
+
+//     if (data.badge.includes("contributions welcome")) {
+//         fs.appendFile(filename, contributions + '\n', function (err) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 console.log("Logged: contributions");
+//             }
+//         });
+//     };
+//     if (data.badge.includes("start with why")) {
+//         fs.appendFile(filename, why + '\n', function (err) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 console.log("Logged: why");
+//             }
+//         });
+//     };
+//     if (data.badge.includes("repo size")) {
+//         fs.appendFile(filename, repo + '\n', function (err) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 console.log("Logged: repo");
+//             }
+//         });
+//     };
+//     // if (data.badge.includes("dependencies")) {
+//     //     badges.push(depend);
+//     // };
+
+//     console.log(badges);
+//     // console.log(badges[0]);
+//     // console.log(badges[1]);
+//     // console.log(badges[2]);
+//     // // console.log(badges[3]);
+
+//     // fs.appendFile(filename, badges[0] + '\n', function (err) {
+//     //     if (err) {
+//     //         console.log(err);
+//     //     }
+//     //     else {
+//     //         console.log("Logged: " + badges[0]);
+//     //     }
+//     // })
+
+//     // fs.appendFile(filename, badges[1] + '\n', function (err) {
+//     //     if (err) {
+//     //         console.log(err);
+//     //     }
+//     //     else {
+//     //         console.log("Logged: " + badges[1]);
+//     //     }
+//     // })
+//     // fs.appendFile(filename, badges[2] + '\n', function (err) {
+//     //     if (err) {
+//     //         console.log(err);
+//     //     }
+//     //     else {
+//     //         console.log("Logged: " + badges[2]);
+//     //     }
+//     // })
+//     // fs.appendFile(filename, badges[3] + '\n', function (err) {
+//     //     if (err) {
+//     //         console.log(err);
+//     //     }
+//     //     else {
+//     //         console.log("Logged: " + badges[3]);
+//     //     }
+//     // })
+
+
+// if(badges.length>0){
+//     for(var i=0; i < badges.length; i++){
+//         fs.appendFile(filename, badges[i] +'\n', function(err){
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 console.log("Logged: "+ badges[i]);
+//             }
+//         } )
+//     }
+// };
+
+
+//     // if (data.badge[0] === "contributions welcome" && data.badge[1] === "start with why") {
+//     //     fs.writeFile(filename, contributions + '\n' + why + '\n' + "# " + data.project_title + '\n', function (err) {
+//     //         if (err) {
+//     //             console.log(err);
+//     //         }
+//     //         else {
+//     //             console.log("Badges and title logged!");
+//     //         }
+//     //     });
+//     // }
+//     // else if (data.badge[0] === "contributions welcome") {
+
+//     //     fs.writeFile(filename, contributions + '\n' + "# " + data.project_title + '\n', function (err) {
+//     //         if (err) {
+//     //             console.log(err);
+//     //         }
+//     //         else {
+//     //             console.log("Contribution badge and title logged!");
+//     //         }
+//     //     })
+//     // }
+//     // else if (data.badge[0] === "start with why") {
+//     //     fs.writeFile(filename, why + '\n' + "# " + data.project_title + '\n', function (err) {
+//     //         if (err) {
+//     //             console.log(err);
+//     //         }
+//     //         else {
+//     //             console.log("Why badge and title logged!");
+//     //         }
+//     //     })
+//     // }
+//     // else {
+//     //     fs.writeFile(filename, "# " + data.project_title + '\n', function (err) {
+//     //         if (err) {
+//     //             console.log(err);
+//     //         }
+//     //         else {
+//     //             console.log("Title logged!");
+//     //         }
+//     //     })
+//     // }
+//     // fs.appendFile(filename, "## " + "Description" + '\n' + data.description + '\n', function (err) {
+//     //     if (err) {
+//     //         console.log(err);
+//     //     }
+//     //     else {
+//     //         console.log("Description logged!");
+//     //     }
+//     // })
+
+//     fs.appendFile(filename, "# " + data.project_title + '\n' + "## " + "Description" + '\n' + data.description + '\n' + "## " + "Table of Contents" + '\n' + "* [Installation](#installation)" + '\n' + "* [Usage](#usage)" + '\n' + "* [License](#license)" + '\n' + "* [Contributing](#contributing)" + '\n' + "* [Tests](#tests)" + '\n' + "* [Questions](#questions)" + '\n' + "## " + "Installation" + '\n' + data.install + '\n' + "## " + "Usage" + '\n' + data.usage + '\n' + "## " + "License" + '\n' + data.license + '\n' + "## " + "Contributing" + '\n' + data.contributing + '\n' + "## " + "Tests" + '\n' + data.tests + '\n' + "## " + "Questions" + '\n' + data.questions + '\n', function (err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             console.log("Table of contents, installation, usage, license, contributing, tests, and questions logged!");
+//         }
+//     })
+//     //     * [Installation](#installation)
+//     // * [Usage](#usage)
+//     // * [License](#license)
+//     // * [Contributing](#contributing)
+//     // * [Tests](#tests)
+//     // * [Questions](#questions)
+//     // + "## " + "Installation" + '\n' + data.install + '\n'
+// })
